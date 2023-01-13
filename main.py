f89f5f932079785e8aa86e0a0f684bcb568786f1
Adrian Nowicki <93824235+Fluffyguy611@users.noreply.github.com>
2023-01-13 04:29:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import hashTable
 import time
 
 if __name__ == '__main__':
-    ht = hashTable.HashTable(1000)
+    ht = hashTable.HashTable(27)
     hs = hashTable.HashTable(500)
     hy = hashTable.HashTable(100)
-    hq = hashTable.HashTable(50)
+    hq = hashTable.HashTable(80)
     with open("nazwiska.txt") as file:
         Lines = file.readlines()
 
@@ -17,8 +17,10 @@ if __name__ == '__main__':
     start_time = time.time()
     for v, k in KVtable:
         ht.insert(k, int(v))
-    print("Deleted objects per deletion: ", ht.DEL)
     print("--- %s seconds --- Hashtable 1000" % (time.time() - start_time))
+    print("Pozycji usunietych: ", ht.DEL)
+    print("Pozycje zapełnione ", ht.filled)
+    print("Dlugosc nazwisk.txt: ", len(Lines))
 
     start_time = time.time()
     for v, k in KVtable:
