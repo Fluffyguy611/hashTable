da1761161cda42c4c9610a6c14f59f4fa610d9e9
Adrian Nowicki <93824235+Fluffyguy611@users.noreply.github.com>
2023-02-06 11:28:39 +0100

    Update hashTable.py

--- hashTable.py
+++ hashTable.py
@@ -5,24 +5,25 @@ class HashTable:
         self.filled = 0
         self.table = [None] * self.size
         self.order = []
-        self.DEL = 0
 
     def hashFunction(self, word):
         hash_value = len(word)
         for ch in word:
-            hash_value += ord(ch)
+            hash_value *= ord(ch)
 
         return hash_value % self.size
 
     def insert(self, key, value):
+        DEL = "DEL"
+
         if self.ifFullDelete():
             self.deleteEverySecond()
+
         hash_value = self.hashFunction(key)
         i = 1
-        while self.table[hash_value] is not None:
-            if i >= self.size*2:
-
-                raise OverflowError("Hashing is taking too long", i, self.filled)
+        while not (self.table[hash_value] is None or self.table[hash_value] == DEL):
+            if i >= self.size*100:
+                raise OverflowError("Hashing is taking too long", i, self.filled, self.table)
 
             # Quadratic probing to find the next available slot
             hash_value = (hash_value + i**2) % self.size
@@ -30,7 +31,6 @@ class HashTable:
         self.table[hash_value] = (key, value)
         self.order.append(hash_value)
         self.filled += 1
-
 
     def search(self, key):
         # Search for the given key in the hash table
@@ -45,13 +45,14 @@ class HashTable:
         return None
 
     def delete(self, key):
+        DEL = "DEL"
         # Delete the given key from the hash table
         hash_value = self.hashFunction(key)
         i = 1
         while True:
             if self.table[hash_value] is not None:
                 if self.table[hash_value][0] == key:
-                    self.table[hash_value] = None
+                    self.table[hash_value] = DEL
                     self.order.remove(hash_value)
                     self.filled -= 1
                     return
@@ -65,13 +66,11 @@ class HashTable:
         return False
 
     def deleteEverySecond(self):
+        DEL = "DEL"
         # Delete every second key from the hash table
         lst = self.order.copy()
-        DEL  = 0
         for i, slot in enumerate(lst):
-            if self.table[slot] is not None and i % 2 == 1:
+            if not (self.table[slot] is None or self.table[slot] == DEL) and i % 2 == 1:
                 delete = self.table[slot][0]
-                DEL += 1
                 self.delete(delete)
 
-        self.DEL = DEL
